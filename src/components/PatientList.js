4562a7bdac7004dfee9c0b1cb218cd4affd58e9e
tivie <estevao.santos@gmail.com>
2021-11-02 04:24:05 +0000

    feat: botão de remover paciente

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -19,7 +19,7 @@ function PatientList({htmlId}) {
   return (
     <nav id={htmlId} className="nav-internados nav nav-fill flex-column">
       {patients.map( ({id, nome}) => (
-        <Link to={`/patient/${id}`} key={id} id={id} component={PatientListItem}>{nome}</Link>
+        <Link to={`/patient/${id}`} key={id} id={id} nome={nome} component={PatientListItem}>{nome}</Link>
       ))}
     </nav>
   );
--- src/components/PatientListItem.js
+++ src/components/PatientListItem.js
@@ -1,25 +1,52 @@
-import React from 'react';
 import {useRouteMatch} from "react-router-dom";
-//import {useHistory} from "react-router-dom";
+import {Button} from "react-bootstrap";
 import './PatientListItem.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrashAlt} from "@fortawesome/free-solid-svg-icons";
+import db from "../db";
 
 /**
  * 
- * @param id
- * @param children
- * @param href
+ * @param {int} id
+ * @param {string} children
+ * @param {string} href
+ * @param {string} nome
  * @returns {JSX.Element}
  * @constructor
  */
-const PatientListItem = ({id, children, href}) => {
+const PatientListItem = ({id, children, href, nome}) => {
   
   let match = useRouteMatch({
     path: href.substring(1),
     exact: true
   });
+
+  /**
+   * 
+   * @param {Event} e
+   * @returns {Promise<void>}
+   */
+  const deletePaciente = async function (e) {
+    e.preventDefault();
+    let retVal = window.confirm(`Tem a certeza que deseja remove o paciente ${nome}`);
+    if (retVal === true) {
+      // noinspection JSCheckFunctionSignatures
+      await db.Pacientes.delete(parseInt(id));
+      alert('Paciente removido com sucesso');
+    }
+  };
   
   return (
-    <a className={`patient-list-item nav-link text-truncate ${match ? 'active' : ''}`} data-id={id} href={href}>{children}</a>
+    <a className={`patient-list-item nav-link text-truncate ${match ? 'active' : ''}`} data-id={id} href={href}>
+      <div className="nav-link-txt">{children}</div>
+      {match ? (
+        <div className="nav-link-del-btn">
+          <Button variant="outline-danger" onClick={deletePaciente}>
+            <FontAwesomeIcon icon={faTrashAlt}/>
+          </Button>
+        </div>
+      ) : null}
+    </a>
   );
 };
 
